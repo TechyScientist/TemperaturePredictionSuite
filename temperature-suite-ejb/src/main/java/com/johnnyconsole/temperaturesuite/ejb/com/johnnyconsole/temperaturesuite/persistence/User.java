cbf9b1096f33d9991057fb80b1bc2bc288b75756
Johnny Console <johnny.console@hotmail.com>
2024-09-24 11:57:10 -0400

    Create User Persistence Object

---
+++ temperature-suite-ejb/src/main/java/com/johnnyconsole/temperaturesuite/ejb/com/johnnyconsole/temperaturesuite/persistence/User.java
@@ -0,0 +1,62 @@
+package com.johnnyconsole.temperaturesuite.ejb.com.johnnyconsole.temperaturesuite.persistence;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="temperature-suite-users")
+@NamedQueries({
+        @NamedQuery(name="User.FindByUsername", query="SELECT u FROM User u WHERE u.username = :username")
+})
+public class User {
+
+    @Id
+    private String username;
+    private String name, password;
+    private int accessLevel;
+
+    public User() {}
+
+    public User(String username, String name, String password, int accessLevel) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.accessLevel = accessLevel;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAccessLevel() {
+        return accessLevel;
+    }
+
+    public void setAccessLevel(int accessLevel) {
+        this.accessLevel = accessLevel;
+    }
+
+    @Override
+    public String toString() {
+        return "User[username=" + username + ", name=" + name + ", accessLevel=" + accessLevel + "]";
+    }
+}
