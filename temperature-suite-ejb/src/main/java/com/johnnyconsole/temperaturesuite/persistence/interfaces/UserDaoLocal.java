2e881c3bba09e7962e5a6e3d93a5dcf40fde7953
Johnny Console <johnny.console@hotmail.com>
2024-09-24 12:07:25 -0400

    Create User Bean Interfaces

---
+++ temperature-suite-ejb/src/main/java/com/johnnyconsole/temperaturesuite/persistence/interfaces/UserDaoLocal.java
@@ -0,0 +1,15 @@
+package com.johnnyconsole.temperaturesuite.persistence.interfaces;
+
+import com.johnnyconsole.temperaturesuite.persistence.User;
+
+import javax.ejb.Local;
+
+@Local
+public interface UserDaoLocal {
+
+    User getUser(String username);
+    boolean addUser(User user);
+    boolean removeUser(User user, String myUsername);
+    boolean verifyUser(String username, String passwordPlainText);
+
+}
